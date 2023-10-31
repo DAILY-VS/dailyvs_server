0f67d9e4b6bcc6afbb80344cce3217698eb0d617
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-10-31 22:41:57 +0900

    #137 poll detail previous_choice 오타 수정

--- vote/views.py
+++ vote/views.py
@@ -105,7 +105,7 @@ class PollDetailView(APIView):
                     category_remove_list.append(category_name)
         category_list = [category for category in category_list if category not in category_remove_list]
         context = {
-            "previous choice" : previous_choice,
+            "previous_choice" : previous_choice,
             "poll": serialized_poll,
             "category_list" : category_list,
         }
