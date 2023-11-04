107af871e3adac4e36b830c010d93d0af4a744bd
spark2357 <psbted@naver.com>
2023-11-04 12:02:58 +0900

    #186 fix: delete decorator

--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,6 @@ from allauth.socialaccount.providers.kakao import views as kakao_view
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from vote.models import Poll, UserVote
 
-@api_view(['GET'])
 class KakaoLoginView:
     def post(self, request):
         code = request.GET.get('code')
