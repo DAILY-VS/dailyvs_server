42df4ff406262c9756bb5e85d0751ca912cfe5cf
hasoleee <wsw3727@gmail.com>
2023-11-04 19:30:27 +0900

    리액트 연동

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ MEDIA_URL = "/media/"
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
