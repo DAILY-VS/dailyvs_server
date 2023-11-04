b9275c31b31f262d49ca2af2d43e3b3a7ef92c73
hasoleee <wsw3727@gmail.com>
2023-11-04 16:37:36 +0900

    settings.py 수정

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ env = environ.Env(
     DEBUG=(bool, False),
 )
 
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 environ.Env.read_env(BASE_DIR / '.env')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -137,7 +137,6 @@ AUTH_USER_MODEL = "accounts.User"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "client/static")]
