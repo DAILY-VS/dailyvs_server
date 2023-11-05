a4f93651247732630b89c300c36eed5b1afd5fa3
hasoleee <wsw3727@gmail.com>
2023-11-05 17:15:07 +0900

    배포 전 코드 변경

--- config/urls.py
+++ config/urls.py
@@ -4,13 +4,15 @@ from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.views.static import serve
+from django.urls import re_path as url
 
 urlpatterns = [
     path("admin/", admin.site.urls), 
     path("api/", include("vote.urls")), #vote app
     path("api/accounts/", include("accounts.urls")),
-    re_path(r'^static/(?P<path>.*)$', serve, { 'document_root': settings.STATIC_ROOT }),
-    re_path('^((?!media).)*$', TemplateView.as_view(template_name='index.html')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url('^((?!media).)*$', TemplateView.as_view(template_name='index.html')),
 ]
 
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
