755b95bed8c262aea16689fd740562b154713e65
hasoleee <wsw3727@gmail.com>
2023-11-05 19:49:20 +0900

    세팅 변경

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ from django.views.static import serve
 from django.urls import re_path as url
 
 urlpatterns = [
-    path("admin/", admin.site.urls), 
+    path("api/admin/", admin.site.urls), 
     path("api/", include("vote.urls")), #vote app
     path("api/accounts/", include("accounts.urls")),
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
