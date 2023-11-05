9cb4d630885529424368512c2df3bf60ccf3700c
hasoleee <wsw3727@gmail.com>
2023-11-06 00:07:50 +0900

    1

--- config/settings.py
+++ config/settings.py
@@ -14,8 +14,9 @@ DEBUG = False
 BASE_DIR = Path(__file__).resolve().parent.parent
 ROOT_DIR = os.path.dirname(BASE_DIR)
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
