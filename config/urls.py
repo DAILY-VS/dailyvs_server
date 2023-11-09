3069d8527435ed8598a916a1152723d00dad199b
hasoleee <wsw3727@gmail.com>
2023-11-09 16:48:43 +0900

    #205 fix: urls.py 수정

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls), 
-    path("", include("vote.urls")), #vote app
-    path("accounts/", include("accounts.urls")),
+    path("api/", include("vote.urls")), #vote app
+    path("api/accounts/", include("accounts.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
