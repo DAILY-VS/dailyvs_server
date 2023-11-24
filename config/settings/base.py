d5de968e61171c7771cbf79b08467ce419c1105f
hasoleee <wsw3727@gmail.com>
2023-11-24 11:27:04 +0900

    api 막음

--- config/settings/base.py
+++ config/settings/base.py
@@ -60,13 +60,13 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'config.CustomMiddleware.DisableCSRFMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    #'config.CustomMiddleware.SuperUserMiddleware',
+    'config.CustomMiddleware.SuperUserMiddleware',
 ]
 
 # CORS 추가
