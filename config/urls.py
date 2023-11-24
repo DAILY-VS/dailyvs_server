7bfe5f732fcf8e7850bc220e5abcc7922c4b7c57
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-11-24 17:37:52 +0900

    #295 fix:api 해쉬 함수로 변경

--- config/urls.py
+++ config/urls.py
@@ -7,9 +7,9 @@ from django.views.static import serve
 from django.urls import re_path as url
 
 urlpatterns = [
-    path("api/admin/", admin.site.urls), 
-    path("api/", include("vote.urls")), #vote app
-    path("api/accounts/", include("accounts.urls")),
+    path("2F650631682090A674BBB3CDBD3A080381C2E37B4B58DB40878FE5AD94635D4E/admin/", admin.site.urls), 
+    path("2F650631682090A674BBB3CDBD3A080381C2E37B4B58DB40878FE5AD94635D4E/", include("vote.urls")), #vote app
+    path("2F650631682090A674BBB3CDBD3A080381C2E37B4B58DB40878FE5AD94635D4E/accounts/", include("accounts.urls")),
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     url('^((?!media).)*$', TemplateView.as_view(template_name='index.html')),
