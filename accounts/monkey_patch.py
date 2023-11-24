0b9583b8e01cb3c473f6ccf795f01c71e0d6645e
spark2357 <psbted@naver.com>
2023-11-24 18:48:48 +0900

    #294 fix: add monkey patch for overriding build_absolute_uri in allauth.utils.py

---
+++ accounts/monkey_patch.py
@@ -0,0 +1,21 @@
+from allauth import utils
+from config.settings import base
+from config.settings.base import local_settings
+from urllib.parse import urlsplit
+
+def monkey_patching():
+    def custom_build_absolute_uri(request, location, protocol=None):
+        site = local_settings.FRONT_BASE_URL
+
+        bits = urlsplit(location)
+        if not (bits.scheme and bits.netloc):
+            uri = "{domain}{url}".format(
+                domain=site,
+                url=location,
+            )
+        else:
+            uri = location
+
+        return uri
+
+    utils.build_absolute_uri = custom_build_absolute_uri
