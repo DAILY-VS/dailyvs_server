415d9d5f053e8ef2d9e785ccd51b3f09882869ce
hasoleee <wsw3727@gmail.com>
2023-12-04 23:51:44 +0900

    develop merge

--- config/settings/base.py
+++ config/settings/base.py
@@ -138,6 +138,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+FILE_UPLOAD_MAX_MEMORY_SIZE = 5242880
 
 if DEBUG == True:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
